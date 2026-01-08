412fdc7a76740ceef04806af2f093ade9a8cabba
Jeremy Marube <jeremymarube1@gmail.com>
2026-01-08 21:51:49 +0300

    Remove unused components and utility functions for cleaner codebase

--- client/src/components/common/LoadingSpinner.jsx
+++
@@ -1,5 +0,0 @@
-export const LoadingSpinner = () => (
-  <div className="flex justify-center py-6">
-    <div className="h-8 w-8 animate-spin rounded-full border-4 border-indigo-500 border-t-transparent" />
-  </div>
-);
